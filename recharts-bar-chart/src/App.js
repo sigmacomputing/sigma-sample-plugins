daa46c96ed1b4f9a4e171b86beae2caf2975d57a
Nana Mimura <nana.mimura@sigmacomputing.com>
2021-09-27 17:36:29 -0700

    add ResponsiveContainer, assign x-axis datakey and bars datakeys to column names

--- recharts-bar-chart/src/App.js
+++ recharts-bar-chart/src/App.js
@@ -1,7 +1,13 @@
-import { client, useConfig, useElementData } from "@sigmacomputing/plugin";
+import {
+  client,
+  useConfig,
+  useElementData,
+  useElementColumns,
+} from "@sigmacomputing/plugin";
 import "./App.css";
 import React from "react";
 import {
+  ResponsiveContainer,
   BarChart,
   Bar,
   XAxis,
@@ -20,36 +26,41 @@ client.config.configureEditorPanel([
     allowMultiple: false,
   },
   {
-    name: "measure 1",
+    name: "measure1",
     type: "column",
     source: "source",
     allowMultiple: false,
   },
-  { name: "measure 2", type: "column", source: "source", allowMultiple: false },
+  { name: "measure2", type: "column", source: "source", allowMultiple: false },
 ]);
 
 function RenderBarChart(props) {
   return (
-    <BarChart
-      width={1800}
-      height={600}
-      data={props.data}
-      margin={{
-        top: 5,
-        right: 50,
-        left: 30,
-        bottom: 90,
-      }}
-      barCategoryGap={5}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="State" angle={-45} textAnchor="end" interval={0} />
-      <YAxis />
-      <Tooltip />
-      <Legend verticalAlign="top" wrapperStyle={{ lineHeight: "40px" }} />
-      <Bar dataKey="Total Pop in 2000" fill="#8884d8" />
-      <Bar dataKey="Total Pop in 2010" fill="#82ca9d" />
-    </BarChart>
+    <ResponsiveContainer width="200%" height={500}>
+      <BarChart
+        data={props.data}
+        margin={{
+          top: 5,
+          right: 50,
+          left: 30,
+          bottom: 90,
+        }}
+        barCategoryGap={5}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey={props.xAxisDataKey}
+          angle={-45}
+          textAnchor="end"
+          interval={0}
+        />
+        <YAxis />
+        <Tooltip />
+        <Legend verticalAlign="top" wrapperStyle={{ lineHeight: "40px" }} />
+        <Bar dataKey={props.bar1DataKey} fill="#8884d8" />
+        <Bar dataKey={props.bar2DataKey} fill="#82ca9d" />
+      </BarChart>
+    </ResponsiveContainer>
   );
 }
 
@@ -57,30 +68,48 @@ function App() {
   const config = useConfig();
 
   const sigmaData = useElementData(config.source);
+  const columns = useElementColumns(config.source);
 
   const dimension = sigmaData[config["dimension"]];
-  const measure1 = sigmaData[config["measure 1"]];
-  const measure2 = sigmaData[config["measure 2"]];
-  console.log(measure1);
-  console.log(measure2);
+  const measure1 = sigmaData[config["measure1"]];
+  const measure2 = sigmaData[config["measure2"]];
+
+  if (dimension && measure1 && measure2) {
+    const dimId = config.dimension;
+    var dimName = columns[dimId].name;
+
+    const m1Id = config.measure1;
+    var m1Name = columns[m1Id].name;
+
+    const m2Id = config.measure2;
+    var m2Name = columns[m2Id].name;
+    //console.log(columns[m2Id].name);
+  }
 
   const data = React.useMemo(() => {
     const data = [];
 
-    if (dimension) {
+    if (dimension && measure1 && measure2) {
       for (let i = 0; i < dimension.length; i++) {
-        let row = {
-          State: dimension[i],
-          "Total Pop in 2000": measure1[i],
-          "Total Pop in 2010": measure2[i],
-        };
+        let row = {};
+        row[dimName] = dimension[i];
+        row[m1Name] = measure1[i];
+        row[m2Name] = measure2[i];
+
         data.push(row);
       }
     }
     return data;
-  }, [dimension, measure1, measure2]);
+  }, [dimension, measure1, measure2, dimName, m1Name, m2Name]);
 
-  return <RenderBarChart data={data} />;
+  return (
+    <RenderBarChart
+      data={data}
+      xAxisDataKey={dimName}
+      bar1DataKey={m1Name}
+      bar2DataKey={m2Name}
+    />
+  );
 }
 
 export default App;
