3a4b2a76be95cf3be02faf98fe6da87cf8d20f6b
Xiaoyu Yin <xiaoyu@xiaoyuy01-sigma.local>
2022-04-20 11:02:03 -0700

    fix clustering map build

--- clustering-map/src/index.js
+++ clustering-map/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import mapboxgl from 'mapbox-gl';
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 ReactDOM.render(
   <React.StrictMode>
