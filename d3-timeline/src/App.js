d5e5389c2541986727d1cdeb013182118af853f8
Xiaoyu Yin <xiaoyu@xiaoyuy01-sigma.local>
2022-05-18 20:34:18 -0700

    use a better color scale

--- d3-timeline/src/App.js
+++ d3-timeline/src/App.js
@@ -1,23 +1,30 @@
 import "./App.css";
 import * as d3 from "d3";
-import p from 'aggregatejs/percentile';
-import { useConfig, useEditorPanelConfig, useElementColumns, useElementData } from '@sigmacomputing/plugin';
+import p from "aggregatejs/percentile";
+import {
+  useConfig,
+  useEditorPanelConfig,
+  useElementColumns,
+  useElementData,
+} from "@sigmacomputing/plugin";
 import timeline from "timelines-chart";
 import { useEffect, useRef } from "react";
 
-const SPAN_START = /^(.*)StartTime$/
-const SPAN_END = /^(.*)EndTime$/
-
+const SPAN_START = /^(.*)StartTime$/;
+const SPAN_END = /^(.*)EndTime$/;
 
 function transform(marks, data, columnInfo, percentile) {
   const aggregatedData = {};
   for (const colId of marks) {
     const colValues = data[colId];
     if (!colValues) continue;
-    aggregatedData[colId] = p(colValues.filter(v => v != null), percentile);
+    aggregatedData[colId] = p(
+      colValues.filter((v) => v != null),
+      percentile
+    );
   }
   const timeSpans = {};
-  let domain = [0,0];
+  let domain = [0, 0];
   for (const colId of marks) {
     const colInfo = columnInfo[colId];
     if (!colInfo) continue;
@@ -28,35 +35,41 @@ function transform(marks, data, columnInfo, percentile) {
         timeSpans[spanStartMatch[1]] = [0, 0];
       }
       timeSpans[spanStartMatch[1]][0] = aggregatedData[colId];
-      domain[0] = Math.min(domain[0], aggregatedData[colId])
+      domain[0] = Math.min(domain[0], aggregatedData[colId]);
     } else if (spanEndMatch) {
       if (!(spanEndMatch[1] in timeSpans)) {
         timeSpans[spanEndMatch[1]] = [0, 0];
       }
       timeSpans[spanEndMatch[1]][1] = aggregatedData[colId];
-      domain[1] = Math.max(domain[1], aggregatedData[colId])
+      domain[1] = Math.max(domain[1], aggregatedData[colId]);
     }
   }
-  const out = [{
-    group: '',
-    data: Object.entries(timeSpans).map(
-      ([label, timeRange]) => ({
+  const out = [
+    {
+      group: "",
+      data: Object.entries(timeSpans).map(([label, timeRange]) => ({
         label,
-        data: [{ timeRange, val: (timeRange[1] - timeRange[0]) / (domain[1] - domain[0]) }]
-      })
-    )
-  }];
+        data: [
+          {
+            timeRange,
+            val: (timeRange[1] - timeRange[0]) / (domain[1] - domain[0])
+          },
+        ],
+      })),
+    },
+  ];
   return out;
 }
 
 function renderTimeline(datum, ref) {
   if (!ref.current) return;
-  d3.select(ref.current).selectAll('*').remove();
+  d3.select(ref.current).selectAll("*").remove();
   timeline()
-    .xTickFormat(x => +x)
-    .timeFormat('%Q')
+    .xTickFormat((x) => +x)
+    .timeFormat("%Q")
     .enableAnimations(false)
-    .zColorScale(c => d3.interpolateBlues(c / 2 + 0.5))
+    .zQualitative(false)
+    .zColorScale(d3.scaleLinear().domain([0, 1]).range(['#90c2de', '#08306b']))
     .data(datum)(ref.current);
 }
 
@@ -64,11 +77,16 @@ function App() {
   useEditorPanelConfig([
     { name: "source", type: "element" },
     { name: "marks", type: "column", source: "source", allowMultiple: true },
-    { name: "percentile", type: "dropdown", source: "source", values: [.25, .50, .75, .95]},
+    {
+      name: "percentile",
+      type: "dropdown",
+      source: "source",
+      values: [0.25, 0.5, 0.75, 0.95],
+    },
   ]);
-  const marks = useConfig('marks');
-  const source = useConfig('source');
-  const percentile = useConfig('percentile');
+  const marks = useConfig("marks");
+  const source = useConfig("source");
+  const percentile = useConfig("percentile");
   const columnInfo = useElementColumns(source);
   const data = useElementData(source);
   const ref = useRef();
