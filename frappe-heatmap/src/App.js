7069e60ad29d393027023592b4df392d77fd717e
Riley Crahen <riley.crahen@sigmacomputing.com>
2026-03-12 14:16:51 -0700

    rm extra

--- frappe-heatmap/src/App.js
+++ frappe-heatmap/src/App.js
@@ -45,7 +45,7 @@ function App() {
       start: new Date(start),
       end: new Date(end),
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps -- columns not used in computation
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [columns, config.dimension, config.measures, sigmaData]);
 
   React.useEffect(() => {
@@ -53,7 +53,7 @@ function App() {
       type: 'heatmap',
       data,
     });
-  // eslint-disable-next-line react-hooks/exhaustive-deps -- init chart once on mount
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   React.useEffect(() => {
